d28a6fddab1803d2ea67edb8f948aaee549d60c4
p <gustjr2332@naver.com>
2021-08-29 13:59:22 +0900

    Greedy Alogorithm

--- 3_Greedy/3-2_problem1_largenumber.py
+++ 3_Greedy/3-2_problem1_largenumber.py
@@ -4,34 +4,52 @@
 import time
 
 N,M,K = map(int, input().split())
-max, max2, sum = 0, 0 , 0
+max, max2, sum = 0, 0 ,0
 
 # array = [N]
 array = list(map(int,input().split()))
 # array = [int(x) for x in array]
 
 start = time.time()
-#가장 큰 수 찾기
-for i in range(len(array)):
-    if max < array[i]:
-        max =  array[i]
 
-#두 번째로 큰 수 찾기
-for j in range(len(array)):
-    if max2 < array[j] and array[j] != max:
-        max2 = array[j]
 
-#가장 큰 수 k번 두 번째 큰 수 1번 더하기
-while M != 0:
-    for _ in range(K):
-        sum += max
-        M -= 1
-    sum += max2
-    M-=1
+# array.sort()
+# first = array[N-1]
+# second = array[N-2]
+#
+# result = 0
+#
+# while(M != 0):
+#     for _ in range(K):
+#         if M==0:
+#             break
+#         result +=first
+#         M-=1
+#
+#     if M==0:
+#         break
+#     result+= second
+#     M-=1
+#
+# end = time.time()
+# print(result)
+
+
+#수학 공식을 이용해서 푸는 방법
+array.sort()
+first = array[N-1]
+second = array[N-2]
+
+count = int(M/(K+1)) * K
+count += M % (K+1)
+
+result = 0
+result += count * first
+result += (M-count) * second
+
 end = time.time()
+print(result)
 
-print(sum)
-print(end - start)
 
 #sort()를 사용해서 크기 순으로 정렬했으면, 쉽게 풀 수 있었다.
 #while문 수정
